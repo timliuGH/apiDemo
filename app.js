5725af6a05fada422baa9e6e336dcafa99284d18
timliuGH <timsgithub@gmail.com>
2019-06-30 14:18:18 +0000

    Add search functionality

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ const app = express();
 const port = 8080;
 const ip = "0.0.0.0";
 const request = require('request');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('search');
+});
 
 app.get('/results', (req, res) => {
-    request('http://omdbapi.com/?apikey=thewdb&s=california', (error, response, body) => {
+    let search = req.query.search;
+    let url = "http://omdbapi.com/?apikey=thewdb&s=" + search;
+    request(url, (error, response, body) => {
         if (!error && response.statusCode == 200) {
-            res.send(body);
+            let parsedBody = JSON.parse(body);
+            res.render('results', {data: parsedBody});
         }
     });
 });
-
 
 app.listen(port, ip, () => {
     console.log(`Starting app on ${port}`);
